cfb083b48814a66e30b420c5b7667c50f647bfa8
thomas-mahon <thomas.mahon@loop.colum.edu>
2017-02-28 21:43:16 -0600

    Add Car and sample standard asset car
    standard asset car temporary, it is to be deleted after using as an
    example for development.

---
+++ BattleCars/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField] float maxAccelerationTorque;
+    [SerializeField] float topSpeed;
+    [SerializeField] WheelCollider [] wheelColliders;
+
+
+    Rigidbody rigidbodyUseThis;
+    float verticalInput;
+    float horizontalInput;
+
+	// Use this for initialization
+	void Start () {
+        rigidbodyUseThis = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //TODO get input from the player
+	}
+
+    void FixedUpdate() {
+        Move();
+        Turn();
+    }
+
+    private void Turn() {
+        
+    }
+
+    private void Move() {
+        
+    }
+}
